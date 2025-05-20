4c1d0f2486c96f2bc694a86bfb4d3734dda10971
¨Gustavo <gustavo-machado-fontenele@hotmail.com>
2025-05-20 14:47:33 -0300

    feat: add install command

---
+++ scripts/installProject.sh
@@ -0,0 +1,15 @@
+echo “Setting db container”
+
+pwd
+
+cd docker-volumes/db_volume/
+
+docker build -t clocker-db .
+
+docker run -d -p 3306:3306 clocker-db
+
+cd ../..
+
+echo “Installing backend dependences”
+
+npm install
